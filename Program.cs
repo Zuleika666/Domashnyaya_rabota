dfe201727c60257fd9092bada53072916c35f12d
Anna <annahuanna@inbox.ru>
2022-12-20 19:41:53 +0300

    Задача_0

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 0. сделать квадрат числа
+Console.Clear();
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
