e2bf44eda03df53ca49be61a00bee1a3b333dc4d
Anna <annahuanna@inbox.ru>
2022-12-20 20:21:41 +0300

    От -n до n

---
+++ Primer3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int n = -1 * number;
+while(n < number)
+{
+Console.Write(n + " ");
+n=n+1;
+}
