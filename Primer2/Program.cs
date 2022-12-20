a7caa67d4d92b15794f614d031dcf289091ba420
Anna <annahuanna@inbox.ru>
2022-12-20 20:01:47 +0300

    задача проверка является ли квадрат

---
+++ Primer2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Введите числа а и b:");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if(b*b == a)
+{
+   Console.Write("ДА");
+}
+else
+{
+    Console.Write("НЕТ");
+}
