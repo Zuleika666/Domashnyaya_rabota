61ebea54aec0ee699a20b70ed1c5a3844eea2594
Anna <annahuanna@inbox.ru>
2022-12-20 20:30:25 +0300

    ДЗ Задача 2

---
+++ dz_zadacha2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("Введите числа: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
